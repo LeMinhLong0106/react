9b49e8847475641e463fed9a8dedfaafd758e3b3
LeMinhLong <63239982+LeMinhLong0106@users.noreply.github.com>
2022-09-01 21:38:13 +0700

    child component, props, conditional

---
+++ src/views/Example/childComponent.js
@@ -0,0 +1,47 @@
+import React from "react";
+class ChildComponent extends React.Component {
+    state = {
+        showJobs: false
+    }
+
+    handelShowHide = () => {
+        this.setState({
+            showJobs: !this.state.showJobs
+        })
+    }
+    render() {
+        // console.log('check props:', this.props)
+        let { name, age, arrJob } = this.props; //this.props là 1 object
+        let { showJobs } = this.state;
+        return (
+            <>
+                <div>
+                    {/* props truyền dl từ cha xuống con */}
+                    tôi tên {name} - {age}
+                </div>
+                {/* điều kiện gọn */}
+                {showJobs === false 
+                    ?
+                    <div><button onClick={() => this.handelShowHide()}>Show</button></div>
+                    :
+                    <>
+                        <div className="arrJob-list">
+                            {
+                                arrJob.map((item) => {
+                                    return (
+                                        <div key={item.id}>
+                                            {item.title} - {item.salary}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div><button onClick={() => this.handelShowHide()}>Hide</button></div>
+                    </>
+                }
+            </>
+        );
+    }
+}
+
+export default ChildComponent;
--- src/views/Example/myComponent.js
+++ src/views/Example/myComponent.js
@@ -1,41 +1,54 @@
 import React from "react";
+import ChildComponent from "./childComponent";
 class MyComponent extends React.Component {
     state = {
-        fname: '',
-        lname: ''
+        name: '',
+        age: '',
+        arrJob: [
+            { id: 'id1', title: 'frontend', salary: '1000' },
+            { id: 'id2', title: 'backend', salary: '1200' },
+            { id: 'id3', title: 'tester', salary: '1300' },
+        ]
     }
-    handelOnChangeFName = (e) => {
+    handelOnChangeName = (e) => {
         this.setState({
-            fname: e.target.value
+            name: e.target.value
         })
     }
-    handelOnChangeLName = (e) => {
+    handelOnChangeAge = (e) => {
         this.setState({
-            lname: e.target.value
+            age: e.target.value
         })
     }
 
-    handelSubmit = (e) =>{
+    handelSubmit = (e) => {
         e.preventDefault();
         console.log('data:', this.state)
     }
     render() {
+        console.log('check state:', this.state)
         return (
             <>
                 <form>
-                    <label htmlFor="fname">First name:</label><br />
-                    <input type="text" value={this.state.fname}
-                        onChange={(e) => this.handelOnChangeFName(e)}
+                    <label htmlFor="name">Name:</label><br />
+                    <input type="text" value={this.state.name}
+                        onChange={(e) => this.handelOnChangeName(e)}
                     /><br />
-                    <label htmlFor="lname">Last name:</label><br />
-                    <input type="text" value={this.state.lname}
-                        onChange={(e) => this.handelOnChangeLName(e)}
+                    <label htmlFor="age">Age:</label><br />
+                    <input type="text" value={this.state.age}
+                        onChange={(e) => this.handelOnChangeAge(e)}
                     /><br />
                     <input type="submit" value="Submit"
-                    onClick={(e)=> this.handelSubmit(e)} />
+                        onClick={(e) => this.handelSubmit(e)} />
                 </form>
-            </>
+                <ChildComponent
+                    // bản chất của props là 1 biến (name)
+                    name={this.state.name}
+                    age={this.state.age}
+                    arrJob={this.state.arrJob}
+                />
 
+            </>
         );
     }
 }
